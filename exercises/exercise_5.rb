734fbcef14ceab473fa8d7ba3a77682bd919d3dc
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-08 23:00:31 -0500

    Sum, average,&  chaining sum

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue of all stores is #{@total_revenue}"
+
+@average_annual_revenue = Store.average(:annual_revenue)
+puts "Avergae annual revenue of all stores is #{@average_annual_revenue}"
+
+@stores_revenue_1M = Store.where('annual_revenue >= 1000000').count
+puts "Number of stores that are generating more than 1M annual revenue is #{@stores_revenue_1M}"
