ba973e846ca9fee2b1ff92ea3070a50afc94d9c4
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-09 00:41:17 -0500

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+print "> "
+store_name = gets.chomp
+
+puts "Carries men's clothing? [Y/N]:"
+print "> "
+mens_apparel = gets.chomp.downcase == "y" #any char except Y or y will be considered null
+
+puts "Carries women's clothing? [Y/N]:"
+print "> "
+womens_apparel = gets.chomp.downcase == "y" #any char except Y or y will be considered null
+
+puts "Enter annual revenue:"
+print "> "
+annual_revenue = gets.chomp.to_i
+
+result = Store.create(
+  name: store_name,
+  mens_apparel: mens_apparel,
+  womens_apparel: womens_apparel,
+  annual_revenue: annual_revenue
+  )
+
+puts result.errors.full_messages #https://amzotti.github.io/rails/2015/01/01/how-do-rails-error-messages-work/
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_associated :employees #https://guides.rubyonrails.org/active_record_validations.html#validates-associated
+  validates :name, length: { minimum: 3 } #https://guides.rubyonrails.org/active_record_validations.html#length
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: -1} #https://guides.rubyonrails.org/active_record_validations.html#length
+  validate :must_carry_at_least_one_of_the_men_or_women_apparel
 
+  def must_carry_at_least_one_of_the_men_or_women_apparel #https://guides.rubyonrails.org/active_record_validations.html#custom-methods
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:base, "Store must carry at least one of the men's or women's apparel") #https://guides.rubyonrails.org/active_record_validations.html#custom-methods
+    end
+  end
 end
+
+
