7f9ca276c4762a058de986ff7e0a5f1d304155ed
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-08 23:56:23 -0500

    presence and inclusion in employee class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, :last_name, :store, presence: true # https://guides.rubyonrails.org/active_record_validations.html#presence
+  validates :hourly_rate, inclusion: 40..200 #https://guides.rubyonrails.org/active_record_validations.html#inclusion
 end
